c5b5ad4e3a140092048b3c2f241ebc8d1e689436
imbhj <wnqudgus5565@gmail.com>
2025-12-02 11:46:52 +0900

    Fix: 카운터 기능 구현

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+app.use(cors({
+    // 리액트 로컬 단 프로세스, EC2 프로세스 허용
+    origin: ["http://localhost:3000", "http://3.37.194.186"],
+    methods: ["GET", "POST"],
+    credentials: true
+}))
+
 // 카운터 초기값
 let count = 1;
 
